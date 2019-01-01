72f47ea6624de9d71d2dd8fadc2581b6a9d50026
jaredpattison <33067293+jaredpattison@users.noreply.github.com>
2019-01-01 12:46:47 -0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/header/header.js";
+import Footer from "./components/footer/footer.js";
+import Content from "./components/content/content.js";
+import "./design/reset.scss";
+import "./design/design.scss";
+
+const App = () => {
+  return (
+    <>
+      <Header title="React Design" />
+      <Content />
+      <Footer>
+        <div>&copy; 2018 Code Fellows</div>
+      </Footer>
+    </>
+  );
+};
+
+export default App;
